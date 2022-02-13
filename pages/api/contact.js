8a3ea010184d11e800141185deeee2ef6dfc042e
sheikh-zafar <audios.shkzafar@gmail.com>
2022-02-13 21:30:18 +0530

    commit

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,37 +1,36 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
-export default function (req, res) {
+export default function(req, res) {
   const Timestamp = req.body.Timestamp;
-    const name = req.body.name;
-    const brand = req.body.brand;
-    const contact = req.body.contact;
-    const description = req.body.description;
-    const email = req.body.email;
-  require('dotenv').config()
+  const name = req.body.name;
+  const brand = req.body.brand;
+  const contact = req.body.contact;
+  const description = req.body.description;
+  const email = req.body.email;
+  require("dotenv").config();
 
-  let nodemailer = require('nodemailer')
   const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
-       auth: {
-            user: 'it.hakstime@gmail.com',
-            pass: process.env.PASS,
-         },
+    auth: {
+      user: "it.hakstime@gmail.com",
+      pass: process.env.PASS,
+    },
     secure: true,
   });
   var details =
-      "Timestamp : " +
-      Timestamp +
-      "\n" +
-      "Customer Name : " +
-      name +
-      "\n" +
-      "Description : " +
-      description;
-    var detailEncode = encodeURI(details);
+    "Timestamp : " +
+    Timestamp +
+    "\n" +
+    "Customer Name : " +
+    name +
+    "\n" +
+    "Description : " +
+    description;
+  var detailEncode = encodeURI(details);
   const mailData = {
     from: email,
-    to: [ "it.haksinterlance@gmail.com"],
+    to: ["it.haksinterlance@gmail.com"],
     subject: `Emirates-car.com - Auto Spare Parts Inquiry Received`,
     html: `<div><h4>Hi Auto spare parts response received for</h4>
     <p>Timestamp: ${Timestamp}</p>
@@ -39,15 +38,13 @@ export default function (req, res) {
     <p>Description : ${description}</p>
     <p>WhatsApp Link: https://api.whatsapp.com/send?phone=${contact}&text=${detailEncode}%0AWe%20received%20your%20enquiry%20for%20car%20battery%20for%20above%20vehicle</p>
     </div>`,
-  }
+  };
 
-  transporter.sendMail(mailData, function (err, info) {
-      if(err)
-        console.log(err)
-      else
-        console.log(info);
-  })
+  transporter.sendMail(mailData, function(err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
 
-  console.log(req.body)
-  res.send('success')
+  console.log(req.body);
+  res.send("success");
 }
