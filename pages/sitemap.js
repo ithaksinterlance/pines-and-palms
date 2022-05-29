768432f99447824839c5a6dbf82d252a3ff39713
sheikh-zafar <audios.shkzafar@gmail.com>
2022-05-29 20:27:17 +0400

    commit

---
+++ pages/sitemap.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Sitemap } from "../links";
+
+export default class sitemap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      site: Sitemap,
+    };
+  }
+  render() {
+    return this.state.site.map((p) => {
+      return (
+        <main key={p.links}>
+          {"<url>"}
+          <br />
+          {"<loc>"}
+          {p.links}
+          {"</loc>"}
+          <br />
+          {"<lastmod>"}
+          {new Date().toISOString()}
+          {"</lastmod>"}<br/>
+          {"<changefreq>"}monthly{"</changefreq>"}<br/>
+          {"<priority>"}1.0{"</priority>"}
+          <br />
+          {"</url>"}
+        </main>
+      );
+    });
+  }
+}
