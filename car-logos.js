c7241c50beee871b94204614c64d13567346fe7d
sheikh-zafar <audios.shkzafar@gmail.com>
2023-01-25 11:38:46 +0400

    commit

--- car-logos.js
+++ car-logos.js
@@ -1,49 +1,49 @@
 export const produc = [
   {
     "id": 1,
-    "logo": "ford.png",
+    "logo": "ford.jpg",
     "name": "Ford",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 2,
-    "logo": "chrysler.png",
+    "logo": "chrysler.jpg",
     "name": "Chrysler",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 3,
-    "logo": "citroen.png",
+    "logo": "citroen.jpg",
     "name": "Citroen",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 4,
-    "logo": "hillman.png",
+    "logo": "hillman.jpg",
     "name": "Hillman",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 5,
-    "logo": "chevrolet.png",
+    "logo": "chevrolet.jpg",
     "name": "Chevrolet",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 6,
-    "logo": "cadillac.png",
+    "logo": "cadillac.jpg",
     "name": "Cadillac",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 7,
-    "logo": "bmw.png",
+    "logo": "bmw.jpg",
     "name": "BMW",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 8,
-    "logo": "austin.png",
+    "logo": "austin.jpg",
     "name": "Austin",
     "alt": "Auto spare parts in uae"
   },
@@ -55,517 +55,517 @@ export const produc = [
   },
   {
     "id": 10,
-    "logo": "fillmore.png",
+    "logo": "fillmore.jpg",
     "name": "Fillmore",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 11,
-    "logo": "pontiac.png",
+    "logo": "pontiac.jpg",
     "name": "Pontiac",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 12,
-    "logo": "studebaker.png",
+    "logo": "studebaker.jpg",
     "name": "Studebaker",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 13,
-    "logo": "buick.png",
+    "logo": "buick.jpg",
     "name": "Buick",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 14,
-    "logo": "rambler.png",
+    "logo": "rambler.jpg",
     "name": "Rambler",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 15,
-    "logo": "plymouth.png",
+    "logo": "plymouth.jpg",
     "name": "Plymouth",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 16,
-    "logo": "volkswagon.png",
+    "logo": "volkswagon.jpg",
     "name": "Volkswagon",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 17,
-    "logo": "jensen.png",
+    "logo": "jensen.jpg",
     "name": "Jensen",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 18,
-    "logo": "oldsmobile.png",
+    "logo": "oldsmobile.jpg",
     "name": "Oldsmobile",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 19,
-    "logo": "mercury.png",
+    "logo": "mercury.jpg",
     "name": "Mercury",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 20,
-    "logo": "dodge.png",
+    "logo": "dodge.jpg",
     "name": "Dodge",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 21,
-    "logo": "shelby.png",
+    "logo": "shelby.jpg",
     "name": "Shelby",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 22,
-    "logo": "porsche.png",
+    "logo": "porsche.jpg",
     "name": "Porsche",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 23,
-    "logo": "toyota.png",
+    "logo": "toyota.jpg",
     "name": "Toyota",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 24,
-    "logo": "mercedesbenz.png",
+    "logo": "mercedesbenz.jpg",
     "name": "Mercedes-Benz",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 25,
-    "logo": "mg.png",
+    "logo": "mg.jpg",
     "name": "MG",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 26,
-    "logo": "nissan.png",
+    "logo": "nissan.jpg",
     "name": "Nissan",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 27,
-    "logo": "honda.png",
+    "logo": "honda.jpg",
     "name": "Honda",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 28,
-    "logo": "mazda.png",
+    "logo": "mazda.jpg",
     "name": "Mazda",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 29,
-    "logo": "renault.png",
+    "logo": "renault.jpg",
     "name": "Renault",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 30,
-    "logo": "audi.png",
+    "logo": "audi.jpg",
     "name": "Audi",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 31,
-    "logo": "lincoln.png",
+    "logo": "lincoln.jpg",
     "name": "Lincoln",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 32,
-    "logo": "lotus.png",
+    "logo": "lotus.jpg",
     "name": "Lotus",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 33,
-    "logo": "maserati.png",
+    "logo": "maserati.jpg",
     "name": "Maserati",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 34,
-    "logo": "mitsubishi.png",
+    "logo": "mitsubishi.jpg",
     "name": "Mitsubishi",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 35,
-    "logo": "saab.png",
+    "logo": "saab.jpg",
     "name": "Saab",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 36,
-    "logo": "subaru.png",
+    "logo": "subaru.jpg",
     "name": "Subaru",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 37,
-    "logo": "suzuki.png",
+    "logo": "suzuki.jpg",
     "name": "Suzuki",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 38,
-    "logo": "lamborghini.png",
+    "logo": "lamborghini.jpg",
     "name": "Lamborghini",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 39,
-    "logo": "merkur.png",
+    "logo": "merkur.jpg",
     "name": "Merkur",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 40,
-    "logo": "land_rover.png",
+    "logo": "land_rover.jpg",
     "name": "Land Rover",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 41,
-    "logo": "acura.png",
+    "logo": "acura.jpg",
     "name": "Acura",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 42,
-    "logo": "lexus.png",
+    "logo": "lexus.jpg",
     "name": "Lexus",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 43,
-    "logo": "eagle.png",
+    "logo": "eagle.jpg",
     "name": "Eagle",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 44,
-    "logo": "alfa_romeo.png",
+    "logo": "alfa_romeo.jpg",
     "name": "Alfa Romeo",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 45,
-    "logo": "daihat.png",
+    "logo": "daihat.jpg",
     "name": "Daihatsu",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 46,
-    "logo": "geo.png",
+    "logo": "geo.jpg",
     "name": "Geo",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 47,
-    "logo": "gmc.png",
+    "logo": "gmc.jpg",
     "name": "GMC",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 48,
-    "logo": "hyundai.png",
+    "logo": "hyundai.jpg",
     "name": "Hyundai",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 49,
-    "logo": "infiniti.png",
+    "logo": "infiniti.jpg",
     "name": "Infiniti",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 50,
-    "logo": "isuzu.png",
+    "logo": "isuzu.jpg",
     "name": "Isuzu",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 51,
-    "logo": "jaguar.png",
+    "logo": "jaguar.jpg",
     "name": "Jaguar",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 52,
-    "logo": "jeep.png",
+    "logo": "jeep.jpg",
     "name": "Jeep",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 53,
-    "logo": "saturn.png",
+    "logo": "saturn.jpg",
     "name": "Saturn",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 54,
-    "logo": "volvo.png",
+    "logo": "volvo.jpg",
     "name": "Volvo",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 55,
-    "logo": "hummer.png",
+    "logo": "hummer.jpg",
     "name": "HUMMER",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 56,
-    "logo": "kia.png",
+    "logo": "kia.jpg",
     "name": "Kia",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 57,
-    "logo": "holden.png",
+    "logo": "holden.jpg",
     "name": "Holden",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 58,
-    "logo": "corbin.png",
+    "logo": "corbin.jpg",
     "name": "Corbin",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 59,
-    "logo": "daewoo.png",
+    "logo": "daewoo.jpg",
     "name": "Daewoo",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 60,
-    "logo": "mini.png",
+    "logo": "mini.jpg",
     "name": "MINI",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 61,
-    "logo": "maybach.png",
+    "logo": "maybach.jpg",
     "name": "Maybach",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 62,
-    "logo": "scion.png",
+    "logo": "scion.jpg",
     "name": "Scion",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 63,
-    "logo": "spyker.png",
+    "logo": "spyker.jpg",
     "name": "Spyker",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 64,
-    "logo": "aston_martin.png",
+    "logo": "aston_martin.jpg",
     "name": "Aston Martin",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 65,
-    "logo": "bentley.png",
+    "logo": "bentley.jpg",
     "name": "Bentley",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 66,
-    "logo": "panoz.png",
+    "logo": "panoz.jpg",
     "name": "Panoz",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 67,
-    "logo": "rolls-royce.png",
+    "logo": "rolls-royce.jpg",
     "name": "Rolls-Royce",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 68,
-    "logo": "spyker.png",
+    "logo": "spyker.jpg",
     "name": "Spyker Cars",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 69,
-    "logo": "ferrari.png",
+    "logo": "ferrari.jpg",
     "name": "Ferrari",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 70,
-    "logo": "hummer.png",
+    "logo": "hummer.jpg",
     "name": "Hummer",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 71,
-    "logo": "morgan.png",
+    "logo": "morgan.jpg",
     "name": "Morgan",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 72,
-    "logo": "peugeot.png",
+    "logo": "peugeot.jpg",
     "name": "Peugeot",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 73,
-    "logo": "foose.png",
+    "logo": "foose.jpg",
     "name": "Foose",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 74,
-    "logo": "aptera.png",
+    "logo": "aptera.jpg",
     "name": "Aptera",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 75,
-    "logo": "smart.png",
+    "logo": "smart.jpg",
     "name": "Smart",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 76,
-    "logo": "bugatti.png",
+    "logo": "bugatti.jpg",
     "name": "Bugatti",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 77,
-    "logo": "tesla.png",
+    "logo": "tesla.jpg",
     "name": "Tesla",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 78,
-    "logo": "ram.png",
+    "logo": "ram.jpg",
     "name": "Ram",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 79,
-    "logo": "fiat.png",
+    "logo": "fiat.jpg",
     "name": "Fiat",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 80,
-    "logo": "fiat.png",
+    "logo": "fiat.jpg",
     "name": "Fiat",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 81,
-    "logo": "mclaren.png",
+    "logo": "mclaren.jpg",
     "name": "McLaren",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 82,
-    "logo": "byd.png",
+    "logo": "byd.jpg",
     "name": "BYD",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 83,
-    "logo": "mclaren.png",
+    "logo": "mclaren.jpg",
     "name": "McLaren Automotive",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 84,
-    "logo": "venturi.png",
+    "logo": "venturi.jpg",
     "name": "Mobility Ventures LLC",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 85,
-    "logo": "pagani.png",
+    "logo": "pagani.jpg",
     "name": "Pagani",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 86,
-    "logo": "roush_performance.png",
+    "logo": "roush_performance.jpg",
     "name": "Roush Performance",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 87,
-    "logo": "smart.png",
+    "logo": "smart.jpg",
     "name": "smart",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 88,
-    "logo": "srt.png",
+    "logo": "srt.jpg",
     "name": "SRT",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 89,
-    "logo": "genesis.png",
+    "logo": "genesis.jpg",
     "name": "Genesis",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 90,
-    "logo": "karma.png",
+    "logo": "karma.jpg",
     "name": "Karma",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 91,
-    "logo": "koenigsegg.png",
+    "logo": "koenigsegg.jpg",
     "name": "Koenigsegg",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 92,
-    "logo": "ruf.png",
+    "logo": "ruf.jpg",
     "name": "RUF Automobile",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 93,
-    "logo": "sti.png",
+    "logo": "sti.jpg",
     "name": "STI",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 94,
-    "logo": "polestar.png",
+    "logo": "polestar.jpg",
     "name": "Polestar",
     "alt": "Auto spare parts in uae"
   },
   {
     "id": 95,
-    "logo": "kandi.png",
+    "logo": "kandi.jpg",
     "name": "Kandi",
     "alt": "Auto spare parts in uae"
   }
