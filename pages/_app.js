cab54405ecad3e520a85fd6162c7cdb82b6d6eb0
sheikh-zafar <audios.shkzafar@gmail.com>
2023-01-26 15:10:58 +0400

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -17,35 +17,62 @@ function MyApp({ Component, pageProps }) {
           name="naver-site-verification"
           content="naver0db31bf474cde5117209785bc5c104ab.html"
         />
-        <meta name="application-name" content="Car spare parts App | Emirates-car.com" />
+        <meta
+          name="application-name"
+          content="Car spare parts App | Emirates-car.com"
+        />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Car spare parts App" />
-        <meta name="description" content="Best App for Ordering Car spare parts" />
+        <meta
+          name="description"
+          content="Best App for Ordering Car spare parts"
+        />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#2B5797" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#2563eb" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
         <link rel="shortcut icon" href="/favicon.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:url" content="https://emirates-car.com" />
         <meta name="twitter:title" content="Car spare parts App" />
-        <meta name="twitter:description" content="Best App for Ordering Car spare parts" />
-        <meta name="twitter:image" content="https://emirates-car.com/icon-192x192.png" />
+        <meta
+          name="twitter:description"
+          content="Best App for Ordering Car spare parts"
+        />
+        <meta
+          name="twitter:image"
+          content="https://emirates-car.com/icon-192x192.png"
+        />
         <meta name="twitter:creator" content="@DavidWShadow" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Car spare parts App" />
-        <meta property="og:description" content="Best App for Ordering Car spare parts" />
+        <meta
+          property="og:description"
+          content="Best App for Ordering Car spare parts"
+        />
         <meta property="og:site_name" content="Emirates auto parts" />
         <meta property="og:url" content="https://emirates-car.com" />
-        <meta property="og:image" content="https://emirates-car.com/icon-192x192.png" />
+        <meta
+          property="og:image"
+          content="https://emirates-car.com/icon-192x192.png"
+        />
         <link
           rel="stylesheet"
           href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
