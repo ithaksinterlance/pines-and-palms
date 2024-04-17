bbe4c2e5857df70e984f4519b2c1df9cecabc24c
sheikh-zafar <audios.shkzafar@gmail.com>
2024-04-17 19:46:11 +0400

    commit

--- app/nav.js
+++ app/nav.js
@@ -11,6 +11,7 @@ import {
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import Iconpng from "/icons/favicon-32x32.png"
 
 export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -19,7 +20,7 @@ export default function Navbar() {
     <nav className="flex relative flex-wrap items-center justify-between px-2 py-2 bg-purple-800">
       <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full flex justify-between">
-          <Image src="/icons/icon-72x72.png" width={32} height={32} />
+          <Image src={Iconpng} width={32} height={32} />
           <Link
             href={`/#spare_parts`}
             className="text-md leading-snug inline-block mr-4 py-2 whitespace-nowrap uppercase text-white xs:-ml-2 2xs:ml-1"
